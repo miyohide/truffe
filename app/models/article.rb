e294a87b061bc4d21e3197eb23a7e37c3b39412c
Natsumi Hirano <rubymydear.n@gmail.com>
2014-01-07 01:05:13 +0900

    update annotate

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id           :integer          not null, primary key
+#  heading      :string(255)
+#  content      :text
+#  published_on :date
+#  tags         :string(255)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Article < ActiveRecord::Base
   default_scope :order => 'published_on desc'
   paginates_per 20
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id           :integer          not null, primary key
+#  heading      :string(255)
+#  content      :text
+#  published_on :date
+#  tags         :string(255)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'spec_helper'
 
 describe Article do
