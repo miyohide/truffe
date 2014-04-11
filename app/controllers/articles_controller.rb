6fefccc774f26bdf8dd96cf765563affd2172be6
Natsumi Hirano <rubymydear.n@gmail.com>
2014-04-12 02:32:25 +0900

    add articles edit/destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,4 +21,26 @@ class ArticlesController < ApplicationController
       render'new'
     end
   end
+
+  def edit
+    @page_title = t('articles.title')
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(params[:article])
+      flash[:success] = t('articles.updated')
+      redirect_to :action => 'index'
+    else
+      @page_title = t('articles.title')
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Article.find(params[:id]).destroy
+    flash[:success] = t('articles.deleted')
+    redirect_to articles_path
+  end
 end
