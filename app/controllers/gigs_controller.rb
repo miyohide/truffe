13fab1d95a160fe23d637ff44d8837b48c83bfb6
natsu871 <rubymydear.n@gmail.com>
2014-12-29 03:57:33 +0900

    Refactoring gigs controller

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -3,29 +3,12 @@ class GigsController < ApplicationController
 
   def index
     @page_title = t('gigs.title')
-
-    @gig_year  = unless params[:year] && (params[:year].to_i >= 2010 && params[:year].to_i <= 2200)
-                   Date.today.year
-                 else
-                   params[:year].to_i
-                 end
-    @gig_month = unless params[:month] && params[:month].to_i.between?(1, 12)
-                   Date.today.month
-                 else
-                   params[:month].to_i
-                 end
+    @current_date = params[:target_date].present? ? Date.parse(params[:target_date]) : Date.today
 
     if signed_in?
-      @gigs = Gig.where('EXTRACT(MONTH FROM gig_date) = ? and EXTRACT(YEAR FROM gig_date) = ?', @gig_month, @gig_year).order("start_time ASC").limit(100).offset(0).page(params[:page])
+      @gigs = Gig.where('gig_date >= ? and gig_date <= ?', @current_date.beginning_of_month, @current_date.end_of_month).order("start_time ASC").limit(100).offset(0).page(params[:page])
     else
-      @gigs = Gig.where('visible = ? and EXTRACT(MONTH FROM gig_date) = ? and EXTRACT(YEAR FROM gig_date) = ?', false, @gig_month, @gig_year).order("start_time ASC").limit(100).offset(0).page(params[:page])
-    end
-
-    if @gig_year.nil? || @gig_month.nil?
-      @prev_year,  @next_year  = Date.today.year
-      @prev_month, @next_month = Date.today.month
-    else
-      @current_date = (Date.new(@gig_year, @gig_month))
+      @gigs = Gig.where('visible = ? and gig_date >= ? and gig_date <= ?', false, @current_date.beginning_of_month, @current_date.end_of_month).order("start_time ASC").limit(100).offset(0).page(params[:page])
     end
   end
 
